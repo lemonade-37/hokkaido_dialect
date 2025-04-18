1c9297b96449764802b6bc4f5bf83af280db0311
satou haruka <haruka.satou.37@icloud.com>
2025-04-18 20:54:06 +0900

    Question functionality is tentatively implemented

--- lib/hokkaido_dialect/hokkaido_dialect.rb
+++ lib/hokkaido_dialect/hokkaido_dialect.rb
@@ -8,4 +8,19 @@ module HokkaidoDialect
     { dialect: "なまら", correct_usage: "締めパフェ、なまら甘かった", wrong_usage: "なまらRubyKaigiだね" },
     { dialect: "したっけ", correct_usage: "こないだ雪かきしたっけ、筋肉痛になったさ", wrong_usage: "電気消したっけ" },
   ]
+
+  class Question
+    def self.ask
+      dialect = DIALECTS.sample
+      correct_usage = dialect[:correct_usage]
+      incorrect_usage = dialect[:wrong_usage]
+
+      choices = [correct_usage, incorrect_usage].shuffle
+
+      {
+        question: "次の文章で正しい北海道弁はどっち？\n1. #{choices[0]}\n2. #{choices[1]}",
+        answer: choices.index(correct_usage) + 1
+      }
+    end
+  end
 end
