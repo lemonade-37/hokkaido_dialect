4fb8a16efcb28c0986a870f1b26fb34168a983b1
satou haruka <haruka.satou.37@icloud.com>
2025-04-18 20:04:25 +0900

    first commit

---
+++ lib/hokkaido_dialect.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hokkaido_dialect/version"
+
+module HokkaidoDialect
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/hokkaido_dialect_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe HokkaidoDialect do
+  it "has a version number" do
+    expect(HokkaidoDialect::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
