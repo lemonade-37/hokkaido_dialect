9f3260651638712416e72776cdd5ea7800e71081
satou haruka <haruka.satou.37@icloud.com>
2025-04-18 20:54:00 +0900

    Dialects implemented

---
+++ lib/hokkaido_dialect/hokkaido_dialect.rb
@@ -0,0 +1,11 @@
+module HokkaidoDialect
+  DIALECTS = [
+    { dialect: "べ", correct_usage: "うまいべ？", wrong_usage: "椅子に座るんだべ" },
+    { dialect: "べさ", correct_usage: "大丈夫だべさ", wrong_usage: "バイバイだべさ" },
+    { dialect: "べや", correct_usage: "お前、ちゃんと連絡しろって言ったべや", wrong_usage: "あの人かっこいいべや" },
+    { dialect: "しょや", correct_usage: "ごみ置きっぱなしにしないでって言ったしょや", wrong_usage: "旭川に行くしょや" },
+    { dialect: "しょ", correct_usage: "釧路行きのバスったら次3時間待たないとないしょ", wrong_usage: "こないだ富良野に行ってきたしょ" },
+    { dialect: "なまら", correct_usage: "締めパフェ、なまら甘かった", wrong_usage: "なまらRubyKaigiだね" },
+    { dialect: "したっけ", correct_usage: "こないだ雪かきしたっけ、筋肉痛になったさ", wrong_usage: "電気消したっけ" },
+  ]
+end
