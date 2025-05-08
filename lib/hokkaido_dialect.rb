cd2e9133cca411213ef088faeba570cbc0b7a3d5
satou haruka <haruka.satou.37@icloud.com>
2025-05-08 21:19:42 +0900

    Add require_relative for hokkaido_dialect.rb

--- lib/hokkaido_dialect.rb
+++ lib/hokkaido_dialect.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "hokkaido_dialect/version"
+require_relative "hokkaido_dialect/hokkaido_dialect"
 
 module HokkaidoDialect
   class Error < StandardError; end
